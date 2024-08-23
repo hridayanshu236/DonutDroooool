778ef594209395202813115ab14421b961b59803
Hridayanshu Raj Acharya <148516090+hridayanshu236@users.noreply.github.com>
2024-08-23 09:52:47 +0545

    Added basic cubic obstacles

---
+++ Assets/Scripts/DonutCollision.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class DonutCollision : MonoBehaviour
+{
+    public DonutMovement movement;
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if(collisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+        }
+        
+    }
+}
